be9503e9544fcaa63412e6aa8d9945602d84f9b2
wellington <wellington.h.s.oliveira@hotmail.com>
2026-02-11 19:41:02 -0300

    aula 7 tela de cadastro

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
